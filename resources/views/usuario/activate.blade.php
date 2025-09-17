6bdf3e6b6e463ee2297b1dbb05d7ceb50caaa662
CarlosBarrientoss <carlosabarrientoss@gmail.com>
2025-09-17 14:32:47 -0500

    Autenticación

--- resources/views/usuario/activate.blade.php
+++ resources/views/usuario/activate.blade.php
@@ -1,19 +1,19 @@
 <div class="modal" id="modal-toggle-{{  $reg->id }}" role="dialog" aria-labelledby="exampleModalLabel">
     <div class="modal-dialog">
-        <div class="modal-content bg-danger">
+        <div class="modal-content {{ $reg->activo ? 'bg-success' : 'bg-warning' }}">
             <form action="{{ route('usuarios.toggle', $reg->id) }}" method="post">
                 @csrf
                 @method('PATCH')
                 <div class="modal-header">
-                    <h4>Eliminar registro</h4>
+                    <h4> {{ $reg->activo ? 'Desactivar' : 'Activar' }} registro</h4>
                 </div>
 
                 <div class="modal-body">
-                    ¿Usted desea eliminar el registro de {{$reg->name}} ?
+                    ¿Usted desea {{ $reg->activo ? 'desactivar ' : 'activar ' }} el registro de {{$reg->name}} ?
                 </div>
 
                 <div class="modal-footer">
-                    <button type="submit" class="btn btn-outline-light">Eliminar</button>
+                    <button type="submit" class="btn btn-outline-light">{{ $reg->activo ? 'Desactivar' : 'Activar' }}</button>
                     <button type="button" class="btn btn-outline-light" data-bs-dismiss="modal">Cerrar</button>
                 </div>
 
