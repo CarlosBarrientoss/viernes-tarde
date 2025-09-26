b7481b4dbd23dbf3e10ec63b8fa2ec6778213d6e
CarlosBarrientoss <carlosabarrientoss@gmail.com>
2025-09-26 16:40:23 -0500

    roles

--- resources/views/role/index.blade.php
+++ resources/views/role/index.blade.php
@@ -8,12 +8,12 @@
                 <div class="col-md-12">
                     <div class="card mb-4">
                         <div class="card-header">
-                            <h3 class="card-title">Usuarios</h3>
+                            <h3 class="card-title">Roles</h3>
                         </div>
                         <!-- /.card-header -->
                         <div class="card-body">
                             <div>
-                                <form action="" method="get">
+                                <form action="{{ route('roles.index') }}" method="get">
                                     <div class="input-group">
                                         <input name="texto" type="text" class="form-control" value=""
                                             placeholder="Ingrese texto a buscar">
@@ -21,7 +21,7 @@
                                             <button type="submit" class="btn btn-primary">
                                                 <i class="fas fa-search"></i>
                                                 Buscar</button>
-                                            <a href="{{ route('usuarios.create') }}" class="btn btn-primary">Nuevo</a>
+                                            <a href="{{ route('roles.create') }}" class="btn btn-primary">Nuevo</a>
                                         </div>
                                     </div>
                                 </form>
@@ -38,20 +38,19 @@
                                             <th style="width: 130px">Opciones</th>
                                             <th style="width: 20px">ID</th>
                                             <th>Nombre</th>
-                                            <th>Correo</th>
-                                            <th>Activo</th>
+                                            <th>Permisos</th>
                                         </tr>
                                     </thead>
                                     <tbody>
                                         @if (count($registros) <= 0)
                                             <tr>
-                                                <td colspan="5">No hay registros que coincidan con la busqueda</td>
+                                                <td colspan="4">No hay registros que coincidan con la busqueda</td>
                                             </tr>
                                         @else
                                             @foreach ($registros as $reg)
                                                 <tr>
                                                     <td>
-                                                        <a href="{{ route('usuarios.edit', $reg->id) }}"
+                                                        <a href="{{ route('roles.edit', $reg->id) }}"
                                                             class="btn btn-info btn-sm">
                                                             <i class="bi bi-pencil-fill"></i>
                                                         </a>
@@ -59,26 +58,21 @@
                                                             data-bs-target="#modal-eliminar-{{ $reg->id }}">
                                                             <i class="bi bi-trash-fill"></i>
                                                         </button>
-                                                        <button
-                                                            class="btn {{ $reg->activo ? 'btn-success' : 'btn-warning' }}  btn-sm"
-                                                            data-bs-toggle="modal"
-                                                            data-bs-target="#modal-toggle-{{ $reg->id }}">
-                                                            <i
-                                                                class="bi {{ $reg->activo ? 'bi-check-circle' : 'bi-ban' }} "></i>
-                                                        </button>
                                                     </td>
                                                     <td>{{ $reg->id }}</td>
                                                     <td>{{ $reg->name }}</td>
-                                                    <td>{{ $reg->email }}</td>
                                                     <td>
-                                                        <span class="badge {{ $reg->activo ? 'bg-success' : 'bg-danger' }}">
-                                                            {{ $reg->activo ? 'Activo' :'Inactivo' }}
-                                                        </span>
-
+                                                        @if ($reg->permissions->isNotEmpty())
+                                                            {!! $reg->permissions->pluck('name')->map(function ($name) {
+                                                                    return "<span class='badge bg-primary me-1'>$name</span>";
+                                                                })->implode(' ') !!}
+                                                        @else
+                                                            <span class="badge bg-secondary">Sin permisos</span>
+                                                        @endif
                                                     </td>
+
                                                 </tr>
-                                                @include('usuario.delete')
-                                                @include('usuario.activate')
+                                                @include('role.delete')
                                             @endforeach
                                         @endif
                                     </tbody>
