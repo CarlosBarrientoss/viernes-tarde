e51de9634dc7b62aeed976278bc2dab013df00aa
CarlosBarrientoss <carlosabarrientoss@gmail.com>
2025-09-26 14:44:00 -0500

    viernes tarde

---
+++ app/Http/Controllers/RoleController.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+
+
+class RoleController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(Request $request)
+    {
+        $texto = $request->input('texto'); //variable del texto de busqueda
+        $registros=Role::with('permissions')->where('name', 'like', "%{$texto}%")
+                        ->orderBy('id', 'desc')
+                        ->paginate(2);
+        return view('role.index', compact('registros', 'texto'));
+
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('role.action');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
